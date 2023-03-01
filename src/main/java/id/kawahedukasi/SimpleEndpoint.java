d6dc438e6b584934effe7dbed917dde7ef5f9bfe
fhasnur <fandi.hasnur10@gmail.com>
2023-03-02 00:00:14 +0800

    add: simple endpoint

---
+++ src/main/java/id/kawahedukasi/SimpleEndpoint.java
@@ -0,0 +1,31 @@
+package id.kawahedukasi;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/sample")
+public class SimpleEndpoint {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String get() {
+        return "This is a sample GET endpoint";
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String post() {
+        return "This is a sample POST endpoint";
+    }
+
+    @PUT
+    @Produces(MediaType.TEXT_PLAIN)
+    public String put() {
+        return "This is a sample PUT endpoint";
+    }
+
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    public String delete() {
+        return "This is a sample DELETE endpoint";
+    }
+}
