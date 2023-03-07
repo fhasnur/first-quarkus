85f0ccc6232b10407e112851f1ed2d34aafad8ec
fhasnur <fandi.hasnur10@gmail.com>
2023-03-07 22:23:57 +0800

    add: product controller

---
+++ src/main/java/id/kawahedukasi/endpoint/advance/ProductController.java
@@ -0,0 +1,22 @@
+package id.kawahedukasi.endpoint.advance;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/*
+ * Multiple Controller
+ */
+@Path("/product")
+@Produces(MediaType.TEXT_PLAIN)
+public class ProductController {
+    @GET
+    public String list() {
+        return "Our Product: \n-Gold\n-Silver";
+    }
+
+    @GET
+    @Path("/bestSeller")
+    public String bestSeller() {
+        return "Our Best Seller: Gold 10 gram";
+    }
+}
